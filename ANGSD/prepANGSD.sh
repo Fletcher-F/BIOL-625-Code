755ebd696d454c951f268f81e2f6e1f2bea45a82
Fletcher Falk <145166065+Fletcher-F@users.noreply.github.com>
2026-02-12 18:21:59 -0800

    Update prepANGSD.sh

--- ANGSD/prepANGSD.sh
+++ ANGSD/prepANGSD.sh
@@ -21,19 +21,21 @@ mkdir ANGSD-final-results
 #saf 1 calculates site allele frequency likelihood based on individual genotype likelihoods assuming HWE.
 #-P is threads.
 
+angsd -GL 2 -out ./ANGSD-final-results/genolike -anc ./reference/Egrandis_297_v2.0.fa -nThreads 4 -doGeno 32 -doPost 1 -postCutoff 0.95 -doGlf 2 -doMajorMinor 1 -doMaf 3 -SNP_pval 1e-6 -bam ./bam.filelist
+#Output Beagle, maf, post, and binary posterior geno
+
+angsd -bam ./bam.filelist -doSaf 1 -out ./ANGSD-final-results/siteafreq -anc ./reference/Egrandis_297_v2.0.fa -GL 2 -P 4 -minMapQ 1 -minQ 20
+#Output saf file
+
 angsd -GL 2 -out ./ANGSD-final-results/readablegeno -nThreads 4 -doGeno 4 -doPost 1 -doMaf 3 -doMajorMinor 1 -postCutoff 0.95 -SNP_pval 1e-6 -bam ./bam.filelist
 #Output geno in human readable format (called genotype AA, AC, AG)
 #needs -doMaf to run so slightly redudant will generate two maf files
 #-doPost 1 estimates posterior genotype probability based on allele freuqency as prior
 
-angsd -bam ./bam.filelist -doSaf 1 -out ./ANGSD-final-results/siteafreq -anc ./reference/Egrandis_297_v2.0.fa -GL 2 -P 4 -minMapQ 1 -minQ 20
-#Output saf file
-
-angsd -GL 2 -out ./ANGSD-final-results/genolike -anc ./reference/Egrandis_297_v2.0.fa -nThreads 4 -doGeno 32 -doPost 1 -postCutoff 0.95 -doGlf 2 -doMajorMinor 1 -doMaf 3 -SNP_pval 1e-6 -bam ./bam.filelist
-#Output Beagle, maf, post, and binary posterior geno
 #GL 1 for SAMtools likelihood (filtering required?). Maf 2 does fixed major unknown minor alleles
 #Note: Highly recommend users don't perform analysis on called genotypes unless they have high depth, as likely bias downstream analysis
 #Maf 3 = estimating the allele frequencies both while assuming known major and minor allele but also while taking the uncertaincy of the minor allele inference
+#I dont think this actually works though and believe it defaults to Maf 1
 
 
 
