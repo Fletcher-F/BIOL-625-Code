97279a079dfb43b68fd2c5a7d8a4c88ef4925056
Fletcher Falk <145166065+Fletcher-F@users.noreply.github.com>
2026-03-03 13:41:33 -0800

    Update ANGSDmix.sh for output paths and resources

--- PopulationGenomics/ANGSDmix.sh
+++ PopulationGenomics/ANGSDmix.sh
@@ -2,14 +2,15 @@
 #ANGSD script for preparing downstream analysis files
 
 #SBATCH --job-name=ANGSDadmixture
-#SBATCH --output=angsdmix.out
-#SBATCH --error=angsdmix.err
-#SBATCH --cpus-per-task=2
-#SBATCH --time=48:00:00
-#SBATCH --mem=128G
+#SBATCH --output=/home/ffalk/links/scratch/angsdmix.out
+#SBATCH --error=/home/ffalk/links/scratch/angsdmix.err
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=48
+#SBATCH --time=12:00:00
+#SBATCH --account=def-jazjanes-ab
 
 module load angsd
 
-for K in 1 2 3 4 5 6; do
-	NGSadmix -likes genolike.beagle.gz -K $K -o k${K}-admix -P 2
+for K in 3 4 5 6; do
+        NGSadmix -likes genolike.beagle.gz -K $K -o /home/ffalk/links/scratch/k${K}-admix -P 48
 done
